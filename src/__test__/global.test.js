9ccd2832e5ffd589392215955af750deb8b2b82f
brahianf <brahianf@unicauca.edu.co>
2020-08-09 18:52:21 -0500

    pruebas para Boolean y Array

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,28 @@
 const text = "Im brahianf"
+const arr = ["food1","food2","foot3"]
 
 test('debe contener string', () => {
     expect(text).toMatch(/brahianf/)
+});
+
+test('Tener food2', () => {
+    expect(arr).toContain('food2')
+});
+
+test('Mayor que', () => {
+    expect(10).toBeGreaterThan(9);
+});
+
+test('Verdadero', () => {
+    expect(true).toBeTruthy()
+});
+
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+}
+
+test('Probar Callback', () => {
+    reverseString('Hell', (str) => {
+        expect(str).toBe('lleH')
+    })
 });
