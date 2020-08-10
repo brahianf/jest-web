a4998e5d82d486d8fef1af51d5d2533a72b5bbf9
brahianf <brahianf@unicauca.edu.co>
2020-08-09 19:12:40 -0500

    pruebas a promesas, before after test

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,30 @@ test('Probar Callback', () => {
         expect(str).toBe('lleH')
     })
 });
+
+const reverseString2 = str => {
+    return new Promise ((resolve, reject) => {
+        if(!str){
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('Probar una promesa', () => {
+    return reverseString2('Hell')
+        .then(string => {
+            expect(string).toBe('lleH')
+        })
+});
+
+test('Probar Async/await', async () => {
+    const string = await reverseString2('Hell');
+    expect(string).toBe('lleH')
+});
+
+afterEach(() => {console.log('after Each test')});
+afterAll(() =>console.log('after all test'))
+
+beforeEach(() => console.group('before each text'))
+beforeAll(() => console.group('before all text'))
