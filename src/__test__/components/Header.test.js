6ed6dddce2353e83914aec1f734b14b618da2404
brahianf <brahianf@unicauca.edu.co>
2020-08-10 01:29:17 -0500

    Snapshot

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme'
+import {create} from 'react-test-renderer'
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
@@ -22,4 +23,15 @@ describe('<Header />', () => {
         )
         expect(header.find('.Header-title').text()).toEqual('Platzi Store')
     })
+});
+
+describe('Header SnapShot', () => {
+    test('Comprobar el SnapShot de Header', () => {
+        const header = create(
+            <ProviderMock>
+                <Header />
+            </ProviderMock>
+        );
+        expect(header.toJSON()).toMatchSnapshot();
+    });
 });
